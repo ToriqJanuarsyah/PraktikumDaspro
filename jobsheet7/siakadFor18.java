1e2a9c744741b30d863a37186c7304c55286a6b9
ToriqJanuarsyah <toriqjanuarsyah@gmail.com>
2025-10-23 11:30:19 +0700

    Percobaan 1

---
+++ jobsheet7/siakadFor18.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class siakadFor18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukan Nilai Mahasiswa ke-" + i + " : ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        
+         System.out.println("Nilai Tertinggi : " + tertinggi);
+         System.out.println("Nilai Terendah : " + terendah);
+    }
+}
