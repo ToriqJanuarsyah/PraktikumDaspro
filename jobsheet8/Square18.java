389f98ff2571cb36c84b104d9fd63f051dad2f59
ToriqJanuarsyah <toriqjanuarsyah@gmail.com>
2025-11-04 22:30:51 +0700

    Percobaan 2

---
+++ jobsheet8/Square18.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Square18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukan Nilai n : ");
+        int n = sc.nextInt();
+
+        for (int iouter = 1; iouter <= n; iouter++ ) {
+            for ( int i = 1; i <= n; i++) {
+            System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
