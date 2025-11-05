c921fb1612852f4d60a56eb32046b83d78c2e7ec
ToriqJanuarsyah <toriqjanuarsyah@gmail.com>
2025-11-06 02:35:35 +0700

    Tugas 1

---
+++ jobsheet8/jmlhKuadratBil.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class jmlhKuadratBil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Masukkan n = ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int jumlah = 0;  
+
+            for (int j = 1; j <= i; j++) {
+                jumlah += (j * j);
+            }
+
+            System.out.print("n = " + i + " -> jumlah kuadrat = ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j * j);
+                if (j < i) {
+                    System.out.print(" + ");
+                }
+            }
+            System.out.println(" = " + jumlah); 
+        }
+    }
+}
