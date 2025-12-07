62ce092f30346a92ecc0f2749f16f2ff360fbf7e
ToriqJanuarsyah <toriqjanuarsyah@gmail.com>
2025-12-07 09:11:51 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe18.java
+++ jobsheet11/PengunjungCafe18.java
@@ -1,14 +1,12 @@
 public class PengunjungCafe18 {
     static void daftarPengunjung (String... namaPengunjung) {
         System.out.println("Daftar nama pengunjung : ");
-        for (int i = 0; i < namaPengunjung.length; i++ ) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+        System.out.println("- " + nama);
         }
     }
 
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
-        daftarPengunjung("Andi");
-        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+        daftarPengunjung();
     }
 }
